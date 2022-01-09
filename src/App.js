03deef389b807c39747cda3b485df96f7300445d
TonyOuma <kyrietony@gmail.com>
2022-01-09 13:47:40 +0300

    core: install packages && configure app navigation

--- src/App.js
+++ src/App.js
@@ -2,21 +2,49 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { ToastProvider, useToast } from "react-native-toast-notifications";
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 // import { style as tw } from 'tailwind-react-native-classnames';
 
 import HomeScreen from './screens/HomeScreen';
+import GoalDetails from './screens/GoalDetails';
 
 const App = () => {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <SafeAreaProvider>
-      {/* <StatusBar
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <ToastProvider placement="top">
+          {/* <StatusBar
         // animated={true}
         backgroundColor="#7F99C7"
         // barStyle={statusBarStyle}
         // showHideTransition={statusBarTransition} hidden={hidden}
          /> */}
-      <HomeScreen />
-    </SafeAreaProvider>
+          <Stack.Navigator>
+
+            <Stack.Screen
+              component={HomeScreen}
+              name="HomeScreen"
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              component={GoalDetails}
+              name="GoalDetails"
+              options={{ headerShown: false }}
+            />
+
+          </Stack.Navigator>
+        </ToastProvider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 };
 
